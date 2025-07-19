9f8b7321df40d6d5a438b294eb8c694d7ce4619a
Junior Silva <zezinhojun@gmail.com>
2025-07-19 16:19:55 -0300

    feat: configs iniciais de conexao typeorm com postegres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './config/typeorm.config';
+import { TesteModule } from './modules/teste/teste.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TesteModule],
   controllers: [AppController],
   providers: [AppService],
 })
